cd36abf8b379ba18842f1792e816ba8b7753a606
ariel363454 <kirito1070100@gmail.com>
2024-12-12 15:30:54 +0800

    Update main4.c

--- EX4/main4.c
+++ EX4/main4.c
@@ -2,7 +2,7 @@
 
 int main() {
   int before=12345;
-  int a, b, c, d, after;
+  int after;
   
   after=before-(before/1000)%10*1000-(before%10)+(before%10*1000)+(before/1000)%10;
   printf("調換前:%d\n調換後:%d",before,after);
