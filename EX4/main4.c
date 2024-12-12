2324da866e829dc4b6cdfd5956e398200ff3240e
ariel363454 <kirito1070100@gmail.com>
2024-12-12 15:30:22 +0800

    Create main4.c

---
+++ EX4/main4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int before=12345;
+  int a, b, c, d, after;
+  
+  after=before-(before/1000)%10*1000-(before%10)+(before%10*1000)+(before/1000)%10;
+  printf("調換前:%d\n調換後:%d",before,after);
+  return 0;
+}
