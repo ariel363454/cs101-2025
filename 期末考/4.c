a011920c05d9005adfff687c06de480bd4f961d1
ariel363454 <kirito1070100@gmail.com>
2024-12-26 01:05:24 +0800

    Create 4.c

---
+++ 期末考/4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int xy=1;xy<=81;xy++) {
+        if (xy%9==0) {
+            printf("%d*%d=%d\n",xy/9,9,xy);
+            continue;
+        }
+        printf("%d*%d=%d\n",xy/9+1,xy%9,(xy/9+1)*(xy%9));
+    }
+
+    return 0;
+}
